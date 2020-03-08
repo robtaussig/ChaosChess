669648cda4cb1cd52791d485df1232579f98d6dc
Rob Taussig <robert.taussig@gmail.com>
2020-03-08 15:28:00 -0400

    Test download size of icons

--- src/components/Dashboard/components/MainScreenDashboard.tsx
+++ src/components/Dashboard/components/MainScreenDashboard.tsx
@@ -1,12 +1,13 @@
 import React, { FC } from 'react';
 import { useMainScreenDashboardStyles } from './style';
+import 'css.gg/icons/add-r.css';
 
 export const MainScreenDashboard: FC = () => {
   const classes = useMainScreenDashboardStyles({});
 
   return (
     <div className={classes.root}>
-
+      <i className="gg-add-r"/>
     </div>
   );
 };
